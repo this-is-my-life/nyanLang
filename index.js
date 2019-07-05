3ea82f19a467055cc9639d4246c8e173d4d212ef
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-05 21:04:57 +0900

    NYANLANG PUBLISHED

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * @name NyanLang
+ * @description 그냥 심심해서 약빨고 만든거니 신경쓰지 마십시오
+ * @version 1.0.0
+ * @see ./test/index.js
+ * @example
+ * require('nyanlang')
+ * '냐~'.냐.냐핫('냐하~').냐핫('냥체 '+'히잇'.냐.냐.냐앗('아앙'.끄앙+'하악'.끄앙+'하앗'.냐아)+'다냐'+'냐하핫'.끄앙).냐하()
+ */
+
+String.prototype.냐 = String.prototype
+
+String.prototype.냐하 = () => {
+  process.exit()
+}
+
+String.prototype.냐핫 = (문장이다냐) => {
+  console.log(문장이다냐)
+  return String.prototype
+}
+
+String.prototype.냐앗 = eval
+
+String.prototype.냐아 = true
+
+String.prototype.냐냐 = false
+
+String.prototype.끄앙 = '!'
